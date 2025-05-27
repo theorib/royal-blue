339829f322f0d4e75eb5d80751f1bc680baa3591
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-05-27 20:37:17 +0100

    ✅ test: add demo test file to test CI/CD workflow
    - add test_delete_me.py file
    - add a demo test to check CI/CD workflow

---
+++ tests/test_delete_me.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+# @pytest.mark.skip
+@pytest.mark.it(
+    "DELETE ME: This is just a demo test to check CI/CD workflow, please delete when real tests are created"
+)
+def test_delete_me():
+    pass
