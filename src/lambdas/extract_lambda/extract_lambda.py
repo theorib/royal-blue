29b7f259ccf950c3bfda352de817187eb48bc022
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-05-28 21:57:43 +0100

    ✨ feat(extract_lambda): create extract_lambda function skeleton
    - create empty skeleton of extract_lambda function ready for TDD
    - add EmptyDict type to typing_utils.py

---
+++ src/lambdas/extract_lambda/extract_lambda.py
@@ -0,0 +1,5 @@
+from src.typing_utils import EmptyDict
+
+
+def lambda_handler(event: EmptyDict, context: EmptyDict):
+    pass
---
+++ src/typing_utils.py
@@ -0,0 +1,5 @@
+from typing import TypedDict
+
+
+class EmptyDict(TypedDict):
+    pass
