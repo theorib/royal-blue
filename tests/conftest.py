31944d209d7520f84dc2378046994418e77bd1c8
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-05-29 09:21:42 +0100

    ✨feat: add mock aws credentials to conftest.py and fix scope of s3_client fixture (#10)
    * ✅ test: refactor s3_client fixture scope for isolated testing
    
    - change scope from class to function to ensure tests do not share the same s3 client instance
    
    * ✅ test: add mocked aws credentials for moto
    
    - add mocked aws credentials as a fixture for moto to allow tests to run without real AWS credentials as a last case scenario fallback

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,22 @@
-from moto import mock_aws
-import pytest
+import os
+
 import boto3
+import pytest
+from moto import mock_aws
 
 
-@pytest.fixture(scope="class")
-def s3_client():
+@pytest.fixture(scope="module", autouse=True)
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+
+@pytest.fixture(scope="function")
+def s3_client(aws_credentials):
     with mock_aws():
         client = boto3.client("s3", region_name="eu-west-2")
         yield client
