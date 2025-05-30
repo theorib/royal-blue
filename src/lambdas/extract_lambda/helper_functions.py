bf02a6ab82f925904c1f313a08d3c90fb29237b6
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-05-30 11:55:53 +0100

    🐛 fix(extract_lambda): correct nosec comments in get_table_last_updated_timestamp
    - fix incorrect comment format for disabling bandit security check

--- src/lambdas/extract_lambda/helper_functions.py
+++ src/lambdas/extract_lambda/helper_functions.py
@@ -30,9 +30,9 @@ def get_table_names(conn: Connection):
 
 # identifier, literal
 def get_table_last_updated_timestamp(conn: Connection, table_name: str):
-    # nosec start
-    query = f"SELECT MAX(last_updated) as last_updated FROM public.{table_name};"  # nosec: B608
-    # nosec end
+    # #nosec start
+    query = f"SELECT MAX(last_updated) as last_updated FROM public.{table_name};"  # #nosec B608
+    # #nosec end
     rows, column_names = execute_query(conn, query)
 
     last_updated = rows[0][0]
