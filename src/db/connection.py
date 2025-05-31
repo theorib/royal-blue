8f66666663558cf462263afe53887fb6a3f8ad27
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-05-31 19:39:06 +0100

    ✨ feat: implement database connection module
    - 【Dependencies】
      - add `psycopg[binary]` for PostgreSQL database interactions
      - add `python-dotenv` for loading environment variables
    - 【New Module】
      - create `src/db/connection.py` to manage database connections
      - use environment variables for database configuration
      - implement `connect_db` function to establish a connection
      - implement `close_db` function to close the connection
    - 【Tests】
      - create `tests/db/conftest.py` to define fixtures for testing
      - create `tests/db/test_connection.py` to test database connection functionality

---
+++ src/db/connection.py
@@ -0,0 +1,46 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+from psycopg import Connection, Error, connect
+from psycopg.rows import dict_row
+
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+
+def connect_db():
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    host = os.getenv("DB_HOST")
+    dbname = os.getenv("DB_DATABASE")
+    port = os.getenv("DB_PORT")
+    conn = None
+    try:
+        conn = connect(
+            f"user={user} password={password} host={host} dbname={dbname} port={int(port or 0000)}",
+            row_factory=dict_row,  # type: ignore
+            cursor_factory=dict_row,  # type: ignore
+        )
+        conn.autocommit = True
+
+        return conn
+    except Error as error:
+        logger.error(error)
+        raise error
+    except Exception as error:
+        logger.error(error)
+        raise error
+
+
+def close_db(conn: Connection):
+    try:
+        if conn:
+            conn.close()
+    except Error as error:
+        logger.error(error)
+        raise error
+    except Exception as error:
+        logger.error(error)
+        raise error
