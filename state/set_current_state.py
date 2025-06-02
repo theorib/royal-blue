0da2c26cc9711adc0df347b45da909fd1a34094f
Janette <janettemsamuels@gmail.com>
2025-06-02 17:45:26 +0100

    ✨ feat(state): implements state management for data ingestion - adds functions to get, set, and update the current state of data ingestion - state is stored in s3 as a json file - includes tests for each function - removes unused helper file
    Co-authored-by: charleybolton <charleybolton@users.noreply.github.com>

--- state/set_current_state.py
+++ state/set_current_state.py
@@ -10,16 +10,9 @@ def set_current_state(current_state: dict, bucket_name, s3_client):
     Converts the given dictionary to JSON and uploads it to S3 as lambda_state.json.
     """
     file_name = 'lambda_state.json'
-    try:
-        json_data = json.dumps(current_state)
-        add_to_s3_bucket(s3_client, bucket_name, file_name, json_data)
-    except ClientError as e:
-          {
-            "error": {
-                "message": f"{file_name} failed to upload to {bucket_name}: {e}"
-            }
-        }
-          
+    json_data = json.dumps(current_state)
+    return add_to_s3_bucket(s3_client, bucket_name, file_name, json_data)
+
           
           
 
