8adb75303aab647239f3e078634f82828dd325ce
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-06-02 15:34:10 +0100

    ✨ feat(extract_lambda): implement dataframe creation and last updated retrieval - 【feat】create create_data_frame_from_list to create dataframes from lists of dictionaries - 【feat】create get_last_updated_from_raw_table_data to get the last updated timestamp - 【test】add tests for both functions to ensure proper functionality
    Co-authored-by: sxnfer <sxnfer@users.noreply.github.com>

--- src/lambdas/extract_lambda/extract_lambda_utils.py
+++ src/lambdas/extract_lambda/extract_lambda_utils.py
@@ -1,4 +1,6 @@
 import logging
+from datetime import datetime
+from typing import List, Optional
 
 import pandas as pd
 
@@ -6,7 +8,29 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def create_data_frame_from_dict(data: dict) -> pd.DataFrame:
-    table_df = pd.DataFrame(data)
-    logger.debug(table_df)
-    return table_df
+def create_data_frame_from_list(data: List[dict]) -> Optional[pd.DataFrame]:
+    if len(data):
+        return pd.DataFrame(data)
+    else:
+        return None
+
+
+def get_last_updated_from_raw_table_data(rows: List[dict]) -> Optional[datetime]:
+    if not rows:
+        return None
+
+    last_updated_column = "last_updated"
+    list_of_datetimes = []
+
+    # [
+    #     row.get("last_updated")
+    #     for row in rows
+    #     if isinstance(row.get("last_updated"), datetime)
+    # ]
+    for row in rows:
+        if last_updated_column in row:
+            value = row[last_updated_column]
+            if isinstance(value, datetime):
+                list_of_datetimes.append(value)
+
+    return max(list_of_datetimes)
