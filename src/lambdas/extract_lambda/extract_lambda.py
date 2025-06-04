cb47b8ad9ddd03d0d59a8c384b6d1324e98d3ff0
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-06-04 14:58:59 +0100

    ✨ feat(extract_lambda): enhance logging for extract lambda
    - add logging configuration with basic settings
    - include more detailed logs at different stages of the extraction process
    - log the result of the extraction process

--- src/lambdas/extract_lambda/extract_lambda.py
+++ src/lambdas/extract_lambda/extract_lambda.py
@@ -1,4 +1,5 @@
 import logging
+import logging.config
 import os
 from copy import deepcopy
 from datetime import datetime
@@ -20,6 +21,10 @@ from src.utilities.state.get_current_state import get_current_state
 from src.utilities.state.set_current_state import set_current_state
 from src.utilities.typing_utils import EmptyDict
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s: %(message)s",
+)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -65,45 +70,50 @@ def lambda_handler(event: EmptyDict, context: EmptyDict):
     s3_client = boto3.client("s3")
     INGEST_ZONE_BUCKET_NAME = os.environ.get("INGEST_ZONE_BUCKET_NAME")
     LAMBDA_STATE_BUCKET_NAME = os.environ.get("LAMBDA_STATE_BUCKET_NAME")
-
-    # get current_state
-    # if current_state file does not exist INITIALIZE STATE
+    result = {"files_to_process": []}
 
     try:
         with conn:
-            totesys_tables = get_totesys_table_names(conn)
+            logger.info("Starting extraction process for all tables")
 
-            result = {"files_to_process": []}
+            totesys_tables = get_totesys_table_names(conn)
 
             for table_name in totesys_tables:
                 # TODO Logging starting the loop????
 
+                logger.info(f"Starting extraction of {table_name}")
+
                 current_state = get_current_state(s3_client, LAMBDA_STATE_BUCKET_NAME)
 
+                # ------------------------------------------------
                 # TODO encapsulate this into a function
                 # initialize_table_state(current_state, table_name)
-                # should return the updated state or if the table state was already there, leave it untouched and return it
+                # should return the updated state or if the table state was already there, leave it untouched and return it (watch for purity, return a new object)
+                # file: utilities/extract_lambda_utils/initialize_table_state.py
                 if not current_state["ingest_state"].get(table_name):
                     current_state["ingest_state"][table_name] = {
                         "last_updated": None,
                         "ingest_log": [],
                     }
+                # ------------------------------------------------
 
                 current_state_last_updated: datetime | None = current_state[
                     "ingest_state"
                 ][table_name]["last_updated"]
 
                 db_response = get_table_data(
-                    conn, table_name, current_state_last_updated
-                )  # type: ignore
+                    conn,
+                    table_name,  # type: ignore
+                    current_state_last_updated,
+                )
+                extraction_timestamp = datetime.now()
 
                 if db_response.get("error"):
                     raise Exception(db_response["error"]["message"])
-                if not len(db_response["success"]["data"]):
-                    continue
 
-                # TODO Logging this????
-                extraction_timestamp = datetime.now()
+                if not len(db_response["success"]["data"]):
+                    logger.info(f"No new data to extract from table: {table_name}")
+                    continue
 
                 table_data = db_response["success"]["data"]
 
@@ -114,12 +124,14 @@ def lambda_handler(event: EmptyDict, context: EmptyDict):
                 table_df = create_data_frame_from_list(table_data)
                 parquet_file = create_parquet_from_data_frame(table_df)
 
+                # ------------------------------------------------
                 # TODO encapsulate creating file names and keys into a function
-                # create_parquet_metadata(last_updated:datetime, table_name:str)
+                # create_parquet_metadata(new_table_data_last_updated:datetime, table_name:str)
                 # returns (
                 # filename,
                 # key
                 # )
+                # file: utilities/extract_lambda_utils/create_parquet_metadata.py
                 year = new_table_data_last_updated.year
                 month = new_table_data_last_updated.month
                 day = new_table_data_last_updated.day
@@ -129,14 +141,14 @@ def lambda_handler(event: EmptyDict, context: EmptyDict):
 
                 # 2025/06/13/currency_2025-06-13_10-35-20_012023.parquet
                 key = f"{year}/{month}/{day}/{filename}"
+                # ------------------------------------------------
 
                 response = add_file_to_s3_bucket(
                     s3_client, INGEST_ZONE_BUCKET_NAME, key, parquet_file
                 )
 
-                # ! REVIEW
+                # ! REVIEW should I get raw error obhect from add_file_to_s3_bucket function??
                 if response.get("error"):
-                    # logger.error(response["error"]["message"])
                     raise response["error"]["raw_response"]  # type: ignore
 
                 new_state_log_entry = (
@@ -151,8 +163,6 @@ def lambda_handler(event: EmptyDict, context: EmptyDict):
 
                 result["files_to_process"].append(new_state_log_entry)
 
-                #     # ! CHANGE THIS TO CALLING THE SET STATE FUNCTION
-                # # update state
                 updated_state_all = deepcopy(current_state)
                 updated_state_all["ingest_state"][table_name]["last_updated"] = (
                     #  TODO THEO check unbound error when not ignoring types
@@ -166,7 +176,13 @@ def lambda_handler(event: EmptyDict, context: EmptyDict):
                     updated_state_all, LAMBDA_STATE_BUCKET_NAME, s3_client
                 )
 
-            return result
+                logger.info(f"Finish extracting table:{table_name} data")
+
+        # !TODO how can we "pretty print" a logger??
+        logger.info(f"Result of extraction process: {result}")
+        logger.info("End of extraction process for all tables")
+        return result
+
     except Exception as err:
         logger.critical(err)
         raise err
