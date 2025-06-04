b5ed4a5436c5f0b2162bdb16a7ee855249978b7e
Charley Bolton <charleybolton1@gmail.com>
2025-06-04 09:14:01 +0100

    ✨ feat: implement state management
    - 【State Management】:
      - add get_current_state.py to retrieve the current state from S3 bucket
      - add set_current_state.py to set the current state by uploading JSON to S3
      - add typing_utils.py for type definitions
      - implement get_current_state() to fetch and parse lambda_state.json from S3
      - implement set_current_state() to convert dictionary to JSON and upload to S3
    
    - 【Testing】:
      - create test_get_current_state.py, test_set_current_state.py, test_update_state.py for testing
      - create update_state.py

---
+++ state/get_current_state.py
@@ -0,0 +1,32 @@
+import json
+
+from botocore.exceptions import ClientError
+
+from src.utils import get_file_from_s3_bucket
+
+
+def get_current_state(s3_client, bucket_name):
+    """
+    Connects to S3 and retrieves the current state from lambda_state.json.
+    Returns the contents as a Python dictionary.
+    """
+    key = 'lambda_state.json'
+
+    try:
+        content = get_file_from_s3_bucket(s3_client, bucket_name, key)
+        return json.loads(content)
+    
+    except ClientError as e:
+          if e.response["Error"]["Code"] == "NoSuchKey":
+              return {
+                "error": {
+                    "message": f"{e}"
+                }
+            }
+    except json.JSONDecodeError as e:
+         return {
+              "error": {
+                   "message": f"Invalid JSON in {key}: {e}"
+              }
+         }
+
---
+++ state/test_get_current_state.py
@@ -0,0 +1,5 @@
+# Test returns correct message for other s3 errors (ClientErrors from get file)
+# Test returns python dict with the same variables
+# Test returns correct error message for non-exsistent json (nosuchkey)
+# Test returns correct error message for invalid json (decoder)
+
