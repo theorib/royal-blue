dccac4afb6f395ee1ceba254af47f08a26785cf0
sxnfer <sxnfernandes@outlook.com>
2025-06-04 17:15:53 +0100

    ♻️ refactor(dim_staff): transform staff dimension data - Refactor dimension data transformation logic in `dim_staff_transform.py` - Move helper function from `helpers.py` to `dim_staff_transform.py` - Update `dim_staff_transform.py` to merge dataframes and create location string - Rename utility files for better clarity - Add comprehensive unit tests for `dim_staff_transform.py` and `convert_parquet_to_dataframe.py`

--- src/lambdas/transform_lambda/dimensions/dim_staff_transform.py
+++ src/lambdas/transform_lambda/dimensions/dim_staff_transform.py
@@ -1,11 +1,56 @@
+import pandas as pd
 
 
-def dim_staff_dataframe(dataframes):
-    dataframe = extracted_dataframes["staff"].rename(columns={
-        "staff_id": "staff_id",
-        "first_name": "staff_first_name",
-        "last_name": "staff_last_name",
-        "department_name": "staff_department_name",
-        "location": "staff_location",
-        "email_address": "staff_email_address"
-    }).drop_duplicates()
+def dim_staff_dataframe(extracted_dataframes: dict):
+    staff_df = extracted_dataframes.get('staff')
+    departments_df = extracted_dataframes['department']
+    locations_df = extracted_dataframes['address']
+    purchase_order_df = extracted_dataframes['purchase_order']
+
+    if staff_df is None:
+        raise ValueError('Error:')
+    if departments_df is None:
+        raise ValueError('Error:')
+    
+    
+    try:
+        staff_df = staff_df.merge(
+            departments_df[['department_id', 'department_name']],
+            how="left",
+            on="department_id"
+        )
+        
+         # Merge staff ←→ purchase_order to get delivery location ID
+        staff_orders = purchase_order_df[['staff_id', 'agreed_delivery_location_id']].drop_duplicates()
+        staff_df = staff_df.merge(
+            staff_orders,
+            how='left',
+            left_on='staff_id',
+            right_on='staff_id'
+        )
+
+        # Merge location ID ←→ address table
+        staff_df = staff_df.merge(
+            locations_df[['address_id', 'city', 'country']],
+            how='left',
+            left_on='agreed_delivery_location_id',
+            right_on='address_id'
+        )
+
+        # Create a readable location string
+        staff_df['location'] = staff_df['city'].fillna('Unknown') + ', ' + staff_df['country'].fillna('Unknown')
+
+
+        dim_staff = staff_df[[
+            'staff_id',
+            'first_name',
+            'last_name',
+            'department_name',
+            'location',
+            'email_address'
+        ]]
+        print(dim_staff)
+        return dim_staff
+    except Exception as e:
+        raise e
+    
