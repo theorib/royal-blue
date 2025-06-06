9186ba536cd0915c1e1325ad1d36c45e10aa74e7
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-06-06 11:19:20 +0100

    ♻️ refactor: remove redundant lambda_function_arn output
    - 【terraform】remove redundant output

--- terraform/modules/extract_lambda/output.tf
+++ terraform/modules/extract_lambda/output.tf
@@ -1,8 +1,3 @@
 output "lambda" {
   value = aws_lambda_function.lambda
 }
-
-output "lambda_function_arn" {
-  description = "Expose ARN from nested create_lambda module"
-  value       = aws_lambda_function.lambda.arn
-}
