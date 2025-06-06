b008f7b4e0eecd131c3b8fbd753753f90128284f
sxnfer <sxnfernandes@outlook.com>
2025-06-06 15:24:46 +0100

    ✨ feat: transform dataframes into dimensions and facts - transform dataframes into dimensions and facts - create parquet files from dimensions - save parquet files to s3

--- src/lambdas/transform_lambda.py
+++ src/lambdas/transform_lambda.py
@@ -1,4 +1,6 @@
 import logging
+import os
+from datetime import datetime
 from io import BytesIO
 
 import boto3
@@ -15,17 +17,18 @@ from src.utilities.parquets.create_parquet_from_data_frame import (
     create_parquet_from_data_frame,
 )
 from utilities.extract_lambda_utils import create_parquet_metadata
+from utilities.s3.add_file_to_s3_bucket import add_file_to_s3_bucket
 from utilities.s3.get_cache_missing_table import cache_missing_table
 
 REQUIRED_TABLES = {
-    'design',
-    'counterparty',
-    'address',
-    'currency',
-    'staff',
-    'department',
-    'transaction',
-    'sales_order'
+    "design",
+    "counterparty",
+    "address",
+    "currency",
+    "staff",
+    "department",
+    "transaction",
+    "sales_order",
 }
 
 logging.basicConfig(
@@ -35,99 +38,63 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+
 def lambda_handler(event, context):
     logger.info("Starting Transformation Lambda")
-    
+    PROCESSED_BUCKET = os.environ.get("INGEST_ZONE_BUCKET_NAME")
+
     try:
-        s3_client = boto3.client('s3')
-        cached_dataframes: dict[str, pd.DataFrame] = {} 
+        s3_client = boto3.client("s3")
+        cached_dataframes: dict[str, pd.DataFrame] = {}
 
         event_dataframes = extract_dataframes_from_event(s3_client, event)
         missing_tables = REQUIRED_TABLES - set(event_dataframes.keys())
-        
+
         if missing_tables:
             logger.info(f"Cache missing tables from s3: {', '.join(missing_tables)}")
-        
+
         for table in missing_tables:
-            missing_table_dataframe = cache_missing_table(s3_client, event_dataframes, table)
+            missing_table_dataframe = cache_missing_table(
+                s3_client, event_dataframes, table
+            )
             cached_dataframes[table] = missing_table_dataframe
-            
+
         # Combine cached_dataframes with event_dataframes
-        all_dataframes: dict[str, pd.DataFrame] = {**event_dataframes, **cached_dataframes}
-        
-        # Transform dataframes into dimensions -> 
-    
-# import json
-# import logging
+        all_dataframes: dict[str, pd.DataFrame] = {
+            **event_dataframes,
+            **cached_dataframes,
+        }
 
-# # import os
-# # from copy import deepcopy
-# # from datetime import datetime
-# from pprint import pformat, pprint
-
-# # import boto3
-# import orjson
-
-# from src.utilities.typing_utils import EmptyDict
-
-# logging.basicConfig(
-#     level=logging.INFO,
-#     format="%(asctime)s | %(levelname)s: %(message)s",
-# )
-# logger = logging.getLogger(__name__)
-# logger.setLevel(logging.INFO)
-
-
-# def lambda_handler(event: dict, context: EmptyDict):
-#     # s3_client = boto3.client("s3")
-#     # INGEST_ZONE_BUCKET_NAME = os.environ.get("INGEST_ZONE_BUCKET_NAME")
-#     # LAMBDA_STATE_BUCKET_NAME = os.environ.get("LAMBDA_STATE_BUCKET_NAME")
-#     # result = {"files_to_process": []}
-#     files_to_process = orjson.loads(json.dumps(event))
-#     pprint(files_to_process)
-#     logger.info("Starting transform process for files")
-
-#     result = {
-#         "files_to_process": [],
-#         "something_else": "I'm some random value from transform_lambda",
-#     }
-
-#     try:
-#         pass
-
-#     except Exception as err:
-#         logger.critical(err)
-#         raise err
-
-#     logger.info("Result of transform process:\n%s", pformat(result))
-#     return orjson.dumps(result)
-
-
-# if __name__ == "__main__":
-#     result = lambda_handler({}, {})
-
+        # Transform dataframes into dimensions -> facts
         dim_counterparty = dim_counterparty_dataframe(all_dataframes)
         dim_location = dim_location_dataframe(all_dataframes)
         dim_currency = dim_currency_dataframe(all_dataframes)
         dim_staff = dim_staff_dataframe(all_dataframes)
         dim_design = dim_design_dataframe(all_dataframes)
         dim_date = dim_date_dataframe(start_date="20100101", end_date="20351231")
-        
         # fact_df = fact_df()
-        
-        # convert to parquets
-        files = [dim_counterparty, dim_location, dim_currency, dim_staff, dim_design, dim_date]
-        parquets: dict[str, BytesIO] = {}
-        for file in files:
-            result = create_parquet_from_data_frame(file)
-            parquets[file] = result
-        
-            # filename, key = create_parquet_metadata(
-            #     new_table_data_last_updated, file.keys()
-            # )
-            # Save to s3
-        
-        # Log results
+
+        dim_dfs = [
+            dim_counterparty,
+            dim_location,
+            dim_currency,
+            dim_staff,
+            dim_design,
+            dim_date,
+        ]
+        files = []
+
+        # convert to parquets and save into s3
+        for df in dim_dfs:
+            parquet = create_parquet_from_data_frame(df)
+
+            _, key = create_parquet_metadata(datetime.now(), df.keys())
+            add_file_to_s3_bucket(s3_client, PROCESSED_BUCKET, key, parquet)
+            files.append({df: parquet, "key": key})
+
+        logger.info("Transform Complete")
+        return files
+
     except Exception as err:
         logger.critical(f"ERROR: {err}")
         raise err
