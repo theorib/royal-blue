2e431a6daa978ee1e5d9e41eec90aabad0b5e837
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-06-09 16:59:22 +0100

    ♻️ refactor(s3): improve cache missing table function
    - rename function to get_table_data_from_s3
    - update function to receive INGEST_ZONE_BUCKET_NAME as an argument
    - improve docstring to better describe function's purpose
    - rename variables for better readability
    - rename parquet_to_dataframe to create_data_frame_from_parquet

--- src/utilities/s3/get_cache_missing_table.py
+++ src/utilities/s3/get_cache_missing_table.py
@@ -2,14 +2,18 @@ import os
 
 from get_file_from_s3_bucket import get_file_from_s3_bucket
 
-from src.utilities.transform_lambda_utils.convert_parquet_to_dataframe import (
-    parquet_to_dataframe,
+from src.utilities.parquets.create_data_frame_from_parquet import (
+    create_data_frame_from_parquet,
 )
 from utilities.state.get_current_state import get_current_state
 
 INGEST_ZONE_BUCKET_NAME = os.environ.get("INGEST_ZONE_BUCKET_NAME")
 
 
+# ! This function should receive the INGEST_ZONE_BUCKET_NAME bucket as an argument to re-pass it into get_file_from_s3_bucket
+# ? Also this function only gets table data it doesn't handle caching which is handled in the next line. It should be called something like get_table_data_from_s3 maybe?
+# ! The docstring is not really telling me what this function does, it says it's caching things on s3 but it's just retrieving things.
+# ! we need better variable names that makes it easier to understand what this function is doing
 def cache_missing_table(client, extracted_dataframes, table_name):
     """
     Handles failed data extraction by caching the problematic table to S3 for debugging or recovery.
@@ -28,7 +32,7 @@ def cache_missing_table(client, extracted_dataframes, table_name):
 
         key = state["ingest_state"][f"{table_name}"]["ingest_log"][-1]["key"]
 
-        return parquet_to_dataframe(
+        return create_data_frame_from_parquet(
             get_file_from_s3_bucket(client, INGEST_ZONE_BUCKET_NAME, key)
         )
     except Exception as e:
