d7dda4f1d61e3e67154a8c67798305b78762e9ec
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-06-09 16:56:28 +0100

    ♻️ refactor(tests): rename and relocate parquet to dataframe utility
    - rename parquet_to_dataframe to create_data_frame_from_parquet
    - move utility from transform_lambda_utils to parquets directory

--- tests/test_transform_lambda/test_convert_parquet_to_dataframe.py
+++ tests/test_transform_lambda/test_convert_parquet_to_dataframe.py
@@ -3,37 +3,33 @@ from io import BytesIO
 import pandas as pd
 import pytest
 
-from src.utilities.transform_lambda_utils.convert_parquet_to_dataframe import (
-    parquet_to_dataframe,
+from src.utilities.parquets.create_data_frame_from_parquet import (
+    create_data_frame_from_parquet,
 )
 
 
 @pytest.fixture
 def test_dataframe():
-    return pd.DataFrame(
-        {
-            "name": ["Charley", "San", "Oliver"],
-            "favourite_icecream": ["Strawberry", "Chocolate", "Vanilla"],
-        }
-    )
+    return pd.DataFrame({
+        "name": ["Charley", "San", "Oliver"],
+        "favourite_icecream": ["Strawberry", "Chocolate", "Vanilla"],
+    })
 
 
 @pytest.mark.describe("parquet_to_dataframe Utility Function Behaviour")
 class TestCreateDataFramesfromParquets:
     @pytest.mark.it("Should return a valid DataFrame from a Parquet bytes")
     def test_returns_data_in_parquet_format(self, test_dataframe):
-        test_dataframe = pd.DataFrame(
-            {
-                "name": ["Charley", "San", "Oliver"],
-                "favourite_icecream": ["Strawberry", "Chocolate", "Vanilla"],
-            }
-        )
+        test_dataframe = pd.DataFrame({
+            "name": ["Charley", "San", "Oliver"],
+            "favourite_icecream": ["Strawberry", "Chocolate", "Vanilla"],
+        })
 
         buffer = BytesIO()
         test_dataframe.to_parquet(buffer, index=False)
         test_dataframe_parquet = buffer.getvalue()
 
-        result = parquet_to_dataframe(test_dataframe_parquet)
+        result = create_data_frame_from_parquet(test_dataframe_parquet)
 
         assert isinstance(result, pd.DataFrame)
         pd.testing.assert_frame_equal(result, test_dataframe)
@@ -45,7 +41,7 @@ class TestCreateDataFramesfromParquets:
         invalid_test_dataframe = "hello"
 
         with pytest.raises(Exception) as e:
-            parquet_to_dataframe(invalid_test_dataframe)
+            create_data_frame_from_parquet(invalid_test_dataframe)
 
         assert isinstance(e.value, Exception)
         assert str(e.value)
@@ -57,7 +53,7 @@ class TestCreateDataFramesfromParquets:
         empty_test_dataframe = b""
 
         with pytest.raises(Exception) as e:
-            parquet_to_dataframe(empty_test_dataframe)
+            create_data_frame_from_parquet(empty_test_dataframe)
 
         assert isinstance(e.value, Exception)
         assert str(e.value)
