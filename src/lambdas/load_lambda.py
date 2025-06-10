299f9ad271d2f2bc826992148b115b4dd3b98f25
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-06-10 11:34:09 +0100

    ✨ feat(load_lambda): add pseudo code for load lambda
    - add pseudo code for loading data from s3 into the data warehouse
    - add connection to the data warehouse

--- src/lambdas/load_lambda.py
+++ src/lambdas/load_lambda.py
@@ -9,6 +9,7 @@ from pprint import pformat, pprint
 # import boto3
 import orjson
 
+from src.db.connection import connect_db
 from src.utilities.typing_utils import EmptyDict
 
 logging.basicConfig(
@@ -18,22 +19,46 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+# event = {"files_to_process": [
+#     {
+#         "table_name": 'dim_counterparty',
+#         "key": "2022/11/3/counterparty_2022-11-3_14-20-51_563000.parquet"
+#     }
+# ]}
+
 
 def lambda_handler(event: dict, context: EmptyDict):
     # s3_client = boto3.client("s3")
-    # INGEST_ZONE_BUCKET_NAME = os.environ.get("INGEST_ZONE_BUCKET_NAME")
+    # PROCESS_ZONE_BUCKET_NAME = os.environ.get("PROCESS_ZONE_BUCKET_NAME")
     # LAMBDA_STATE_BUCKET_NAME = os.environ.get("LAMBDA_STATE_BUCKET_NAME")
-    # result = {"files_to_process": []}
-    files_to_process = orjson.loads(json.dumps(event))
+    conn = connect_db("DATAWAREHOUSE")
+
+    files_to_process = orjson.loads(json.dumps(event)).get("files_to_process")
     pprint(files_to_process)
     logger.info("Starting transform process for files")
 
-    # !!!!!!!! WE ALWAYS need to insert dimensions first and facts last
+    """
 
-    result = {
-        "files_to_process": [],
-        "something_else": "I'm some random value from load_lambda",
-    }
+    dim_to_process = []
+    facts_to_process = []
+
+    for file_data in file_to_process:
+        # get parquet from s3
+        # convert parquet to df
+        # check from table name if it's dim or fact and add it to either dim_to_process and facts_to_process
+        # make sure that dim_to_process and facts_to_process are a list of dictionaries that contain both the df and the table name
+
+        
+
+    for file_data in dim_to_process:
+        create_db_entries_from_df(conn, file_data['table_name'], file_data['df'])
+        
+    for file_data in facts_to_process:
+        create_db_entries_from_df(conn, file_data['table_name'], file_data['df'])
+
+
+        don't forget to log stuff, like when you start, when you finish, which files were processed into which tables, etc.  
+    """
 
     try:
         pass
