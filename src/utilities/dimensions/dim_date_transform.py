5bd71a6eb603b6c00631eac8809fb7f2ae1badea
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-06-10 11:52:17 +0100

    ✨ feat(dim_date_transform): create dim_date table - add date dimension table creation function - add date_id column - add timezone to Europe/London

--- src/utilities/dimensions/dim_date_transform.py
+++ src/utilities/dimensions/dim_date_transform.py
@@ -2,16 +2,19 @@ import pandas as pd
 
 
 def dim_date_dataframe(start_date: str, end_date: str):
-    all_dates = pd.date_range(start=start_date, end=end_date)
-    date_dataframe = pd.DataFrame({"date_id": all_dates})
+    all_dates = pd.date_range(
+        start=start_date, end=end_date, tz="Europe/London", normalize=True
+    )
+    date_dataframe = pd.DataFrame({"date_value": all_dates})
 
-    date_dataframe["year"] = date_dataframe["date_id"].dt.year
-    date_dataframe["month"] = date_dataframe["date_id"].dt.month
-    date_dataframe["day"] = date_dataframe["date_id"].dt.day
-    date_dataframe["day_of_week"] = date_dataframe["date_id"].dt.weekday + 1
-    date_dataframe["day_name"] = date_dataframe["date_id"].dt.day_name()
-    date_dataframe["month_name"] = date_dataframe["date_id"].dt.month_name()
-    date_dataframe["quarter"] = date_dataframe["date_id"].dt.quarter
+    date_dataframe["year"] = date_dataframe["date_value"].dt.year
+    date_dataframe["month"] = date_dataframe["date_value"].dt.month
+    date_dataframe["day"] = date_dataframe["date_value"].dt.day
+    date_dataframe["day_of_week"] = date_dataframe["date_value"].dt.weekday + 1
+    date_dataframe["day_name"] = date_dataframe["date_value"].dt.day_name()
+    date_dataframe["month_name"] = date_dataframe["date_value"].dt.month_name()
+    date_dataframe["quarter"] = date_dataframe["date_value"].dt.quarter
+    date_dataframe["date_id"] = date_dataframe["date_value"].dt.date
 
     date_dataframe = date_dataframe[
         [
