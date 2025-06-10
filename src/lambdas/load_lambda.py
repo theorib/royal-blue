d977778d352a58b0bbf4c2da02f457809e7fc0d0
sxnfer <sxnfernandes@outlook.com>
2025-06-10 13:57:51 +0100

    ✨ feat(load_lambda): implement load lambda to process files from s3 to db (#53)
    - add boto3 to connect to s3
    - add create_data_frame_from_parquet to create df from parquet files
    - add connect_db to connect to db
    - add create_db_entries_from_df to create db entries from df
    - get files to process from process zone bucket
    - process files into dims and facts
    - create db entries for each dim and fact table
    - log results

--- src/lambdas/load_lambda.py
+++ src/lambdas/load_lambda.py
@@ -1,14 +1,23 @@
 import json
 import logging
+import os
 
 # import os
 # from copy import deepcopy
 # from datetime import datetime
-from pprint import pprint
+from pprint import pformat
+
+import boto3
 
 # import boto3
 import orjson
 
+from src.db.connection import connect_db
+from src.utilities.load_lambda_utils import create_db_entries_from_df
+from src.utilities.parquets.create_data_frame_from_parquet import (
+    create_data_frame_from_parquet,
+)
+from src.utilities.s3.get_file_from_s3_bucket import get_file_from_s3_bucket
 from src.utilities.typing_utils import EmptyDict
 
 logging.basicConfig(
@@ -27,13 +36,12 @@ logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event: dict, context: EmptyDict):
-    # s3_client = boto3.client("s3")
-    # PROCESS_ZONE_BUCKET_NAME = os.environ.get("PROCESS_ZONE_BUCKET_NAME")
+    s3_client = boto3.client("s3")
+    PROCESS_ZONE_BUCKET_NAME = os.environ.get("PROCESS_ZONE_BUCKET_NAME")
     # LAMBDA_STATE_BUCKET_NAME = os.environ.get("LAMBDA_STATE_BUCKET_NAME")
-    # conn = connect_db("DATAWAREHOUSE")
+    conn = connect_db("DATAWAREHOUSE")
 
     files_to_process = orjson.loads(json.dumps(event)).get("files_to_process")
-    pprint(files_to_process)
     logger.info("Starting transform process for files")
 
     """
@@ -59,17 +67,42 @@ def lambda_handler(event: dict, context: EmptyDict):
         don't forget to log stuff, like when you start, when you finish, which files were processed into which tables, etc.  
     """
 
+    if not files_to_process:
+        logger.info("No files to process")
+        return
+
     try:
-        pass
+        dims_to_process = []
+        facts_to_process = []
+
+        for file_data in files_to_process:
+            parquet = get_file_from_s3_bucket(
+                s3_client, bucket_name=PROCESS_ZONE_BUCKET_NAME, key=file_data["key"]
+            )
+            df = create_data_frame_from_parquet(parquet)
+
+            if file_data["table_name"].startswith("dim"):
+                dims_to_process.append(
+                    {"table_name": file_data["table_name"], "dataframe": df}
+                )
+            else:
+                facts_to_process.append(
+                    {"table_name": file_data["table_name"], "dataframe": df}
+                )
+
+        for file_data in dims_to_process:
+            create_db_entries_from_df(conn, file_data["table_name"], file_data["df"])
+
+        for file_data in facts_to_process:
+            create_db_entries_from_df(conn, file_data["table_name"], file_data["df"])
 
     except Exception as err:
         logger.critical(err)
         raise err
 
-    # logger.info("Result of loading process:\n%s", pformat(result))
-    # return orjson.dumps(result)
+    logger.info("Result of loading process:\n%s", pformat(result))
+    return orjson.dumps(result)
 
 
 if __name__ == "__main__":
-    pass
-    # result = lambda_handler({}, {})
+    result = lambda_handler({}, {})
