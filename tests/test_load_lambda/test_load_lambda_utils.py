ffdb1367921e1f3fd130f4d6b952e46756e5fcfd
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-06-10 11:37:09 +0100

    💄 style(test): apply ruff formatting to test_load_lambda_utils.py

--- tests/test_load_lambda/test_load_lambda_utils.py
+++ tests/test_load_lambda/test_load_lambda_utils.py
@@ -8,19 +8,20 @@ from src.utilities.load_lambda_utils import create_db_entries_from_df
 @pytest.mark.describe("Test create_db_entries_from_db utility behaviour")
 class TestCreateDbEntriesFromDf:
     def test_insert_data_into_table(self):
-
-        df = pd.DataFrame({
-            "enrollment_no": [12, 13],
-            "name": ["sarah", "ray"],
-            "science_marks": [90, 81]
-        })
+        df = pd.DataFrame(
+            {
+                "enrollment_no": [12, 13],
+                "name": ["sarah", "ray"],
+                "science_marks": [90, 81],
+            }
+        )
 
         conn = psycopg2.connect(
             database="Classroom",
-            user='postgres',
-            password='pass',
-            host='127.0.0.1',
-            port='5432'
+            user="postgres",
+            password="pass",
+            host="127.0.0.1",
+            port="5432",
         )
 
         create_db_entries_from_df(conn, "classroom", df)
@@ -29,12 +30,9 @@ class TestCreateDbEntriesFromDf:
         cursor.execute("SELECT * FROM classroom")
         rows = cursor.fetchall()
 
-        expected_rows = [(12, "sarah", 90),
-                        (13, "ray", 81)]
+        expected_rows = [(12, "sarah", 90), (13, "ray", 81)]
 
         assert rows == expected_rows
 
         cursor.close()
         conn.close()
-
-
