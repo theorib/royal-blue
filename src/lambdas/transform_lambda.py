71e693d6fb1b4c2321bf4110648d893642a6c6d5
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-06-11 15:04:28 +0100

    ♻️ refactor(transform): refactor lambda logic into separate functions
    - refactor lambda logic into separate functions
    - make it incremental

--- src/lambdas/transform_lambda.py
+++ src/lambdas/transform_lambda.py
@@ -3,7 +3,6 @@ import logging
 import os
 from copy import deepcopy
 from datetime import datetime
-from typing import List
 
 import boto3
 import orjson
@@ -16,20 +15,25 @@ from src.utilities.dimensions.dim_date_transform import dim_date_dataframe
 from src.utilities.dimensions.dim_design_transform import dim_design_dataframe
 from src.utilities.dimensions.dim_location_transform import dim_location_dataframe
 from src.utilities.dimensions.dim_staff_transform import dim_staff_dataframe
-from src.utilities.extract_lambda_utils import create_parquet_metadata
 from src.utilities.facts.create_fact_sales_order_from_df import (
     create_fact_sales_order_from_df,
-)
-from src.utilities.parquets.create_data_frame_from_parquet import (
-    create_data_frame_from_parquet,
 )
 from src.utilities.parquets.create_parquet_from_data_frame import (
     create_parquet_from_data_frame,
 )
-from src.utilities.s3.add_file_to_s3_bucket import add_file_to_s3_bucket
-from src.utilities.s3.get_file_from_s3_bucket import get_file_from_s3_bucket
+from src.utilities.pydantic_models import (
+    FilesToProcessList,
+    State,
+)
 from src.utilities.state.get_current_state import get_current_state
 from src.utilities.state.set_current_state import set_current_state
+from src.utilities.transform_lambda_utils.transform_lambda_utils import (
+    add_log_to_result_and_state,
+    get_dataframes_from_files_to_process,
+    get_log_item_df_s3_upload,
+    initialize_dim_date,
+    initialize_transform_state,
+)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -37,25 +41,6 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
-# event = {
-#     "files_to_process": [
-#         {
-#             "table_name": "counterparty",
-#             "extraction_timestamp": "2025-06-09T09:51:12.338449",
-#             "last_updated": "2022-11-03T14:20:51.563000",
-#             "file_name": "counterparty_2022-11-3_14-20-51_563000.parquet",
-#             "key": "2022/11/3/counterparty_2022-11-3_14-20-51_563000.parquet",
-#         }
-#     ]
-# }
-
-# result = {"files_to_process": [
-#     {
-#         "table_name": 'dim_counterparty',
-#         "key": "2022/11/3/dim_counterparty_2022-11-3_14-20-51_563000.parquet"
-#     }
-# ]}
 
 
 def lambda_handler(event, context):
@@ -66,16 +51,17 @@ def lambda_handler(event, context):
     s3_client = boto3.client("s3")
     logger.info("Starting Transformation Lambda")
 
-    # ! fix datetime not coming in
-    files_to_process: List[dict] = orjson.loads(json.dumps(event)).get(
-        "files_to_process"
+    # # ! fix datetime not coming in
+    files_to_process = FilesToProcessList.validate_python(
+        orjson.loads(json.dumps(event)).get("files_to_process")
     )
-
-    current_state: dict = get_current_state(s3_client, LAMBDA_STATE_BUCKET_NAME)
+    current_state = State.model_validate(
+        get_current_state(s3_client, LAMBDA_STATE_BUCKET_NAME)
+    ).model_dump()
 
     final_state = deepcopy(current_state)
 
-    result = {"files_to_process": []}
+    result: dict[str, list[dict]] = {"files_to_process": []}
 
     if not files_to_process:
         logger.info("Finish Transformation Lambda with no files to process.")
@@ -83,134 +69,82 @@ def lambda_handler(event, context):
 
     if not current_state.get("transform_state", {}).get("last_updated", None):
         logger.info(
-            "Running transform process for the first time. Gathering data from all tables"
+            "Running transform process for the first time. Initializing dim_date table"
         )
 
-        # TODO this function needs to move to it's own file
-        def get_dataframes_from_files_to_process(client, bucket, files_to_process):
-            all_df_to_process = {}
-            for file_data in files_to_process:
-                table_name = file_data["table_name"]
-                key = file_data["key"]
-
-                response = get_file_from_s3_bucket(client, bucket, key)
-
-                parquet = response["success"]["data"]
-
-                df = create_data_frame_from_parquet(parquet)
-
-                all_df_to_process[table_name] = df
-
-            return all_df_to_process
-
-        all_tables_dfs: dict = get_dataframes_from_files_to_process(
-            s3_client, INGEST_ZONE_BUCKET_NAME, files_to_process
+        final_state = initialize_transform_state(
+            final_state, [file.table_name for file in files_to_process]
         )
 
-        # TODO create the dim_dates before anything else
-        # !inject dim_date into files to process
-        inject_dim_date = {
-            "extraction_timestamp": None,
-            "file_name": None,
-            "key": None,
-            "last_updated": datetime.now().isoformat(),
-            "table_name": "dim_date",
-        }
+        initialize_dim_date(
+            create_dim_date_df_func=dim_date_dataframe,  # type: ignore
+            s3_client=s3_client,
+            bucket_name=PROCESS_ZONE_BUCKET_NAME,  # type: ignore
+            result=result,
+            final_state=final_state,
+        )
 
-        files_to_process.append(inject_dim_date)
+    all_tables_dfs: dict = get_dataframes_from_files_to_process(
+        s3_client,
+        INGEST_ZONE_BUCKET_NAME,  # type: ignore
+        files_to_process,
+    )
 
-        # !initialize state
-        final_state["transform_state"] = {"last_updated": None, "tables": {}}
-        if final_state:
-            for file_to_process in files_to_process:
-                table_name: str = file_to_process["table_name"]
-                final_state["transform_state"]["tables"][table_name] = {}
+    for file_to_process in files_to_process:
+        table_name = file_to_process.table_name
+        last_updated = file_to_process.last_updated
 
-        for file_to_process in files_to_process:
-            table_name = file_to_process["table_name"]
-            last_updated = file_to_process["last_updated"]
+        match table_name:
+            case "counterparty":
+                df = dim_counterparty_dataframe(
+                    counterparty=all_tables_dfs["counterparty"],
+                    address=all_tables_dfs["address"],
+                )
+                new_table_name = "dim_counterparty"
+            case "design":
+                df = dim_design_dataframe(
+                    design=all_tables_dfs["design"],
+                )
+                new_table_name = "dim_design"
+            case "currency":
+                df = dim_currency_dataframe(currency=all_tables_dfs["currency"])
+                new_table_name = "dim_currency"
+            case "staff":
+                df = dim_staff_dataframe(
+                    department=all_tables_dfs["department"],
+                    staff=all_tables_dfs["staff"],
+                )
+                new_table_name = "dim_staff"
+            case "dim_date":
+                df = dim_date_dataframe("20221102", "20500101")
+                new_table_name = "dim_date"
+            case "address":
+                df = dim_location_dataframe(address=all_tables_dfs["address"])
+                new_table_name = "dim_location"
+            case "sales_order":
+                df = create_fact_sales_order_from_df(all_tables_dfs["sales_order"])
+                new_table_name = "fact_sales_order"
+            case _:
+                continue
+        transformation_timestamp = datetime.now()
 
-            # ! these are the tables we are still missing fro post mvp
-            # purchase_order
-            # payment_type
-            # payment
-            # transaction
-
-            match table_name:
-                case "counterparty":
-                    df = dim_counterparty_dataframe(
-                        counterparty=all_tables_dfs["counterparty"],
-                        address=all_tables_dfs["address"],
-                    )
-                    new_table_name = "dim_counterparty"
-                case "design":
-                    df = dim_design_dataframe(
-                        design=all_tables_dfs["design"],
-                    )
-                    new_table_name = "dim_design"
-                case "currency":
-                    df = dim_currency_dataframe(currency=all_tables_dfs["currency"])
-                    new_table_name = "dim_currency"
-                case "staff":
-                    df = dim_staff_dataframe(
-                        department=all_tables_dfs["department"],
-                        staff=all_tables_dfs["staff"],
-                    )
-                    new_table_name = "dim_staff"
-                case "dim_date":
-                    df = dim_date_dataframe("20221102", "20500101")
-                    new_table_name = "dim_date"
-                case "address":
-                    df = dim_location_dataframe(address=all_tables_dfs["address"])
-                    new_table_name = "dim_location"
-                case "sales_order":
-                    df = create_fact_sales_order_from_df(all_tables_dfs["sales_order"])
-                    new_table_name = "fact_sales_order"
-                case _:
-                    continue
-
-            parquet = create_parquet_from_data_frame(df)
-
-            filename, key = create_parquet_metadata(
-                # ! we need to fix the json import from event to give us datetime objects
-                datetime.fromisoformat(last_updated),
-                new_table_name,
-            )
-
-            response = add_file_to_s3_bucket(
-                s3_client, PROCESS_ZONE_BUCKET_NAME, key, parquet
-            )
-
-            if response.get("error"):
-                raise Exception("something went wring with uploading")
-
-            transformation_timestamp = datetime.now()
-
-            log_item = {
-                "table_name": new_table_name,
-                "key": key,
-                "filename": filename,
-                # ! we need to fix the json import from event to give us datetime objects
-                "last_updated": datetime.fromisoformat(last_updated),
-                "transformation_timestamp": transformation_timestamp,
-                # TODO take into account that some functions use more than one input
-                # "transformation_input": file_to_process,
-            }
-
-            result["files_to_process"].append(log_item)
-
-            final_state["transform_state"]["tables"][table_name][
-                "transformation_timestamp"
-            ] = transformation_timestamp
-            final_state["transform_state"]["tables"][table_name]["process_log"] = [
-                log_item
-            ]
-
-            final_state["transform_state"]["last_updated"] = transformation_timestamp
-
-    else:
-        # now we only process incrementally
-        pass
+        log_item = get_log_item_df_s3_upload(
+            s3_client=s3_client,
+            bucket_name=PROCESS_ZONE_BUCKET_NAME,  # type: ignore
+            last_updated=last_updated,
+            new_table_name=new_table_name,
+            df=df,
+            transformation_timestamp=transformation_timestamp,
+            create_parquet_from_df_func=create_parquet_from_data_frame,  # type: ignore
+        )
+        add_log_to_result_and_state(
+            log=log_item,
+            result=result,
+            state=final_state,
+            last_updated=transformation_timestamp,
+            processing_timestamp=transformation_timestamp,
+            table_name=table_name,
+        )
 
     set_current_state(final_state, LAMBDA_STATE_BUCKET_NAME, s3_client)
 
@@ -222,77 +156,77 @@ if __name__ == "__main__":
         "files_to_process": [
             {
                 "table_name": "counterparty",
-                "extraction_timestamp": "2025-06-10T20:51:34.260407",
+                "extraction_timestamp": "2025-06-11T13:49:02.425852",
                 "last_updated": "2022-11-03T14:20:51.563000",
                 "file_name": "counterparty_2022-11-3_14-20-51_563000.parquet",
                 "key": "2022/11/3/counterparty_2022-11-3_14-20-51_563000.parquet",
             },
             {
                 "table_name": "address",
-                "extraction_timestamp": "2025-06-10T20:51:37.179714",
+                "extraction_timestamp": "2025-06-11T13:49:06.666745",
                 "last_updated": "2022-11-03T14:20:49.962000",
                 "file_name": "address_2022-11-3_14-20-49_962000.parquet",
                 "key": "2022/11/3/address_2022-11-3_14-20-49_962000.parquet",
             },
             {
                 "table_name": "department",
-                "extraction_timestamp": "2025-06-10T20:51:39.879502",
+                "extraction_timestamp": "2025-06-11T13:49:10.367019",
                 "last_updated": "2022-11-03T14:20:49.962000",
                 "file_name": "department_2022-11-3_14-20-49_962000.parquet",
                 "key": "2022/11/3/department_2022-11-3_14-20-49_962000.parquet",
             },
             {
                 "table_name": "purchase_order",
-                "extraction_timestamp": "2025-06-10T20:51:42.599581",
-                "last_updated": "2025-06-10T16:51:09.789000",
-                "file_name": "purchase_order_2025-6-10_16-51-9_789000.parquet",
-                "key": "2025/6/10/purchase_order_2025-6-10_16-51-9_789000.parquet",
+                "extraction_timestamp": "2025-06-11T13:49:14.569049",
+                "last_updated": "2025-06-11T13:32:09.975000",
+                "file_name": "purchase_order_2025-6-11_13-32-9_975000.parquet",
+                "key": "2025/6/11/purchase_order_2025-6-11_13-32-9_975000.parquet",
             },
             {
                 "table_name": "staff",
-                "extraction_timestamp": "2025-06-10T20:51:46.181541",
+                "extraction_timestamp": "2025-06-11T13:49:19.688113",
                 "last_updated": "2022-11-03T14:20:51.563000",
                 "file_name": "staff_2022-11-3_14-20-51_563000.parquet",
                 "key": "2022/11/3/staff_2022-11-3_14-20-51_563000.parquet",
             },
             {
                 "table_name": "payment_type",
-                "extraction_timestamp": "2025-06-10T20:51:48.938756",
+                "extraction_timestamp": "2025-06-11T13:49:23.627405",
                 "last_updated": "2022-11-03T14:20:49.962000",
                 "file_name": "payment_type_2022-11-3_14-20-49_962000.parquet",
                 "key": "2022/11/3/payment_type_2022-11-3_14-20-49_962000.parquet",
             },
             {
                 "table_name": "payment",
-                "extraction_timestamp": "2025-06-10T20:51:52.380596",
-                "last_updated": "2025-06-10T18:01:10.155000",
-                "file_name": "payment_2025-6-10_18-1-10_155000.parquet",
-                "key": "2025/6/10/payment_2025-6-10_18-1-10_155000.parquet",
+                "extraction_timestamp": "2025-06-11T13:49:28.729186",
+                "last_updated": "2025-06-11T13:36:09.847000",
+                "file_name": "payment_2025-6-11_13-36-9_847000.parquet",
+                "key": "2025/6/11/payment_2025-6-11_13-36-9_847000.parquet",
             },
             {
                 "table_name": "transaction",
-                "extraction_timestamp": "2025-06-10T20:51:59.679060",
-                "last_updated": "2025-06-10T18:01:10.155000",
-                "file_name": "transaction_2025-6-10_18-1-10_155000.parquet",
-                "key": "2025/6/10/transaction_2025-6-10_18-1-10_155000.parquet",
+                "extraction_timestamp": "2025-06-11T13:49:38.765635",
+                "last_updated": "2025-06-11T13:36:09.847000",
+                "file_name": "transaction_2025-6-11_13-36-9_847000.parquet",
+                "key": "2025/6/11/transaction_2025-6-11_13-36-9_847000.parquet",
             },
             {
                 "table_name": "design",
-                "extraction_timestamp": "2025-06-10T20:52:12.199106",
+                "extraction_timestamp": "2025-06-11T13:49:54.025634",
                 "last_updated": "2025-06-10T17:51:09.671000",
                 "file_name": "design_2025-6-10_17-51-9_671000.parquet",
                 "key": "2025/6/10/design_2025-6-10_17-51-9_671000.parquet",
             },
             {
                 "table_name": "sales_order",
-                "extraction_timestamp": "2025-06-10T20:52:15.981714",
-                "last_updated": "2025-06-10T18:01:10.155000",
-                "file_name": "sales_order_2025-6-10_18-1-10_155000.parquet",
-                "key": "2025/6/10/sales_order_2025-6-10_18-1-10_155000.parquet",
+                "extraction_timestamp": "2025-06-11T13:49:59.348065",
+                "last_updated": "2025-06-11T13:36:09.847000",
+                "file_name": "sales_order_2025-6-11_13-36-9_847000.parquet",
+                "key": "2025/6/11/sales_order_2025-6-11_13-36-9_847000.parquet",
             },
             {
                 "table_name": "currency",
-                "extraction_timestamp": "2025-06-10T20:52:22.062585",
+                "extraction_timestamp": "2025-06-11T13:50:07.969299",
                 "last_updated": "2022-11-03T14:20:49.962000",
                 "file_name": "currency_2022-11-3_14-20-49_962000.parquet",
                 "key": "2022/11/3/currency_2022-11-3_14-20-49_962000.parquet",
@@ -300,5 +234,11 @@ if __name__ == "__main__":
         ]
     }
 
-    # result = lambda_handler(test_event, {})
-    # pprint(result)
+    result = lambda_handler(test_event, {})
+    # print(result)
+
+    # ! these are the tables we are still missing fro post mvp
+    # purchase_order
+    # payment_type
+    # payment
+    # transaction
---
+++ src/utilities/transform_lambda_utils/transform_lambda_utils.py
@@ -0,0 +1,136 @@
+from copy import deepcopy
+from datetime import datetime
+from types import FunctionType
+from typing import List
+
+import pandas as pd
+
+from src.utilities.extract_lambda_utils import create_parquet_metadata
+from src.utilities.parquets.create_data_frame_from_parquet import (
+    create_data_frame_from_parquet,
+)
+from src.utilities.parquets.create_parquet_from_data_frame import (
+    create_parquet_from_data_frame,
+)
+from src.utilities.pydantic_models import FilesToProcessItem
+from src.utilities.s3.add_file_to_s3_bucket import add_file_to_s3_bucket
+from src.utilities.s3.get_file_from_s3_bucket import get_file_from_s3_bucket
+
+
+def get_dataframes_from_files_to_process(
+    client, bucket: str, files_to_process: List[FilesToProcessItem]
+):
+    all_df_to_process = {}
+    for file_data in files_to_process:
+        table_name = file_data.table_name
+        key = file_data.key
+
+        response = get_file_from_s3_bucket(client, bucket, key)
+
+        parquet = response["success"]["data"]
+
+        df = create_data_frame_from_parquet(parquet)
+
+        all_df_to_process[table_name] = df
+
+    return all_df_to_process
+
+
+def initialize_transform_state(state, table_names):
+    initialized_state = deepcopy(state)
+    initialized_state["transform_state"] = {"last_updated": None, "tables": {}}
+
+    for table_name in table_names:
+        initialized_state["transform_state"]["tables"][table_name] = {}
+    return initialized_state
+
+
+# mutates result and state
+def add_log_to_result_and_state(
+    log: List[FilesToProcessItem],
+    result,
+    state,
+    table_name: str,
+    last_updated: datetime,
+    processing_timestamp: datetime,
+):
+    result["files_to_process"].append(log)
+    state["transform_state"]["tables"][table_name] = {}
+    state["transform_state"]["tables"][table_name]["transformation_timestamp"] = (
+        processing_timestamp
+    )
+    state["transform_state"]["tables"][table_name]["process_log"] = log
+
+    state["transform_state"]["last_updated"] = last_updated
+
+
+def initialize_dim_date(
+    create_dim_date_df_func: FunctionType,
+    s3_client,
+    bucket_name: str,
+    result: dict,
+    final_state,
+):
+    new_table_name = "dim_date"
+    dim_date_df = create_dim_date_df_func("20221102", "20400101")
+    dim_date_transformation_time_stamp = datetime.now()
+    dim_date_log_item = get_log_item_df_s3_upload(
+        s3_client=s3_client,
+        bucket_name=bucket_name,
+        last_updated=dim_date_transformation_time_stamp,
+        new_table_name=new_table_name,
+        df=dim_date_df,
+        transformation_timestamp=dim_date_transformation_time_stamp,
+        create_parquet_from_df_func=create_parquet_from_data_frame,  # type: ignore
+    )
+
+    add_log_to_result_and_state(
+        log=dim_date_log_item,
+        result=result,
+        state=final_state,
+        last_updated=dim_date_transformation_time_stamp,
+        processing_timestamp=dim_date_transformation_time_stamp,
+        table_name=new_table_name,
+    )
+
+    # result["files_to_process"].append(dim_date_log_item)
+    # final_state["transform_state"]["tables"]["dim_date"] = {}
+    # final_state["transform_state"]["tables"]["dim_date"]["transformation_timestamp"] = (
+    #     dim_date_transformation_time_stamp
+    # )
+    # final_state["transform_state"]["tables"]["dim_date"]["process_log"] = [
+    #     dim_date_log_item
+    # ]
+
+    # final_state["transform_state"]["last_updated"] = dim_date_transformation_time_stamp
+
+
+def get_log_item_df_s3_upload(
+    s3_client,
+    bucket_name: str,
+    last_updated: datetime,
+    new_table_name: str,
+    df: pd.DataFrame,
+    transformation_timestamp: datetime,
+    create_parquet_from_df_func: FunctionType,
+):
+    parquet = create_parquet_from_df_func(df)
+    filename, key = create_parquet_metadata(
+        last_updated,
+        new_table_name,
+    )
+
+    response = add_file_to_s3_bucket(s3_client, bucket_name, key, parquet)
+
+    if response.get("error"):
+        raise Exception("something went wrong while upload")
+
+    log_item = {
+        "table_name": new_table_name,
+        "key": key,
+        "filename": filename,
+        "last_updated": last_updated,
+        "transformation_timestamp": transformation_timestamp,
+    }
+
+    return log_item
