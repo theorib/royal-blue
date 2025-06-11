5c69344ace31f6a7744ddb7c86fed5f1392e52ce
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-06-11 15:04:00 +0100

    ✨ feat: add pydantic models for data validation and structure
    - add pydantic models for:
      - 【EnvironmentVariables】: represents environment variables
      - 【FilesToProcessItem】: represents individual files to be processed
      - 【FilesToProcessList】: type adapter for a list of FilesToProcessItem
      - 【FilesToProcess】: represents a list of files to be processed
      - 【ProcessState】: represents the state of the processing
      - 【State】: represents the overall state including ingest and process states

---
+++ src/utilities/pydantic_models.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from typing import List
+
+from pydantic import BaseModel, TypeAdapter
+
+
+class EnvironmentVariables(BaseModel):
+    environment_variable: str
+
+
+class FilesToProcessItem(BaseModel):
+    table_name: str
+    extraction_timestamp: datetime
+    last_updated: datetime
+    file_name: str
+    key: str
+
+
+FilesToProcessList = TypeAdapter(List[FilesToProcessItem])
+
+
+class FilesToProcess(BaseModel):
+    files_to_process: List[FilesToProcessItem]
+
+
+class ProcessState(BaseModel):
+    last_updated: datetime | None
+    tables: dict
+
+
+class State(BaseModel):
+    ingest_state: dict
+    process_state: ProcessState
